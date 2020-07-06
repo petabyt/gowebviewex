9abc43c7d8250141abb559987e3ac602f54c7a24
Petabyte <brikbusters@gmail.com>
2020-07-06 17:48:58 -0400

    Update webview.go

--- webview.go
+++ webview.go
@@ -11,16 +11,18 @@ func array(array []string) string {
 	return strings.Join(array, "\000")
 }
 
+// Called from Java always at startup
 func Startup() string {
 	settings := []string{
-		"file:///android_asset/index.html",
-		"// Nothin",
+		"file:///android_asset/index.html", // File the webview will go to
+		"// Nothin", // Startup code once the webview has loaded
 	}
 
 	settingsString := array(settings)
 	return settingsString
 }
 
+// Called from Java
 func Test() string {
 	return "Hey Vsauce, Michael here"
 }
